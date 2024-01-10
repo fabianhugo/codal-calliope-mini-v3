89dc9b9720d88d49dc7a8771d215e0d8f77f50b3
fabianhugo <huwg@mailbox.org>
2024-01-10 17:14:14 +0100

    reverts i2c pins, updates compat.h

--- inc/compat/MicroBitCompat.h
+++ inc/compat/MicroBitCompat.h
@@ -348,11 +348,11 @@ const uint16_t MICROBIT_ID_MBED_TICKER       __attribute__ ((deprecated)) = 83;
 #define MICROBIT_ID_IO_P13                                      ID_PIN_P13
 #define MICROBIT_ID_IO_P14                                      ID_PIN_P14
 #define MICROBIT_ID_IO_P15                                      ID_PIN_P15
-#define MICROBIT_ID_IO_A1_RX                                    ID_PIN_P16 //#define MICROBIT_ID_IO_P16
-#define MICROBIT_ID_IO_A1_TX                                    ID_PIN_P17 //#define MICROBIT_ID_IO_P17
-#define MICROBIT_ID_IO_P18                                      ID_PIN_P18
-#define MICROBIT_ID_IO_A0_SCL                                   ID_PIN_P19
-#define MICROBIT_ID_IO_A0_SDA                                   ID_PIN_P20 //#define MICROBIT_ID_IO_P20 
+#define MICROBIT_ID_IO_P16                                      ID_PIN_P16
+#define MICROBIT_ID_IO_P17                                      ID_PIN_P17 //[Calliope] add
+#define MICROBIT_ID_IO_P18                                      ID_PIN_P18 //[Calliope] add
+#define MICROBIT_ID_IO_P19                                      ID_PIN_P19
+#define MICROBIT_ID_IO_P20                                      ID_PIN_P20
 #define MICROBIT_ID_LOGO                                        ID_PIN_LOGO
 
 // Calliope mini3
